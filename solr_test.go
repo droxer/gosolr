10888fddf6e21642bb0cd809a972a4cceab27748
He Fei <droxer.he@gmail.com>
2014-11-21 20:23:46 +0800

    added commit request

--- solr_test.go
+++ solr_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-func TestAddDoc(t *testing.T) {
+func TestAdd(t *testing.T) {
 	var resp = `
                 {
                   "responseHeader": {
@@ -24,7 +24,7 @@ func TestAddDoc(t *testing.T) {
 
 		data, _ := ioutil.ReadAll(req.Body)
 
-		assert.Equal(t, `{"add":{"commitWithin":0,"overwrite":false,"boost":0,"doc":{"documentid":118523475}}}]`, string(data))
+		assert.Equal(t, `{"add":{"commitWithin":0,"overwrite":false,"boost":0,"doc":{"documentid":118523475}}}`, string(data))
 		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte(resp))
 	}))
@@ -39,5 +39,56 @@ func TestAddDoc(t *testing.T) {
 	}
 
 	solrRep, _ := solr.Add(doc, true, false)
+	assert.Equal(t, 200, solrRep.Header.Status)
+}
+
+func TestDeleteById(t *testing.T) {
+	var resp = `
+                {
+                  "responseHeader": {
+                    "status": 200,
+                    "QTime": 0
+                  }
+                }
+            `
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/solr/collection1/update?commit=true&softCommit=false&wt=json", req.RequestURI)
+		assert.Equal(t, "POST", req.Method)
+
+		data, _ := ioutil.ReadAll(req.Body)
+
+		assert.Equal(t, `{"delete":{"id":"123456"}}`, string(data))
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(resp))
+	}))
+	defer ts.Close()
+
+	solr := NewSolr(ts.URL+"/solr/collection1", time.Second*10)
+
+	solrRep, _ := solr.DeleteById("123456", true)
+	assert.Equal(t, 200, solrRep.Header.Status)
+}
+
+func TestCommit(t *testing.T) {
+	var resp = `
+                {
+                  "responseHeader": {
+                    "status": 200,
+                    "QTime": 0
+                  }
+                }
+            `
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/solr/collection1/update?commit=true&softCommit=false&wt=json", req.RequestURI)
+		assert.Equal(t, "GET", req.Method)
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(resp))
+	}))
+	defer ts.Close()
+
+	solr := NewSolr(ts.URL+"/solr/collection1", time.Second*10)
+
+	solrRep, _ := solr.Commit()
 	assert.Equal(t, 200, solrRep.Header.Status)
 }
