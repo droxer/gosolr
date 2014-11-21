b4553f254a9f226fd6f677845f66775b43a4c910
He Fei <droxer.he@gmail.com>
2014-11-21 14:08:25 +0800

    refine solr response.

--- response.go
+++ response.go
@@ -1,13 +1,28 @@
 package gosolr
 
 type SolrResponse struct {
-	Header struct {
-		Status int
-		QTime  int
-	} `json:"responseHeader"`
+	Header   *Header   `json:"responseHeader"`
+	Response *Response `json:"error,omitempty"`
+	Error    Error     `json:"error,omitempty"`
+}
 
-	Error struct {
-		Msg  string
-		Code int
-	} `json:"error"`
+type Header struct {
+	Status int
+	QTime  int
+	Params *Params `json:"params,omitempty"`
+}
+
+type Response struct {
+}
+
+type Error struct {
+	Msg  string
+	Code int
+}
+
+type Params struct {
+	Indent bool
+	Q      string
+	_      string
+	wt     string
 }
